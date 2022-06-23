3558586f1ef07f46c117300e10627fa2b391bbef
ERTWF <106486342+ERTWF@users.noreply.github.com>
2022-06-23 18:42:59 +0300

    Add files via upload

--- NIXA/Player/id.py
+++ NIXA/Player/id.py
@@ -2,14 +2,14 @@ from NIXA.main import bot
 from pyrogram import filters
 
 
-@bot.on_message(filters.command('id'))
+@bot.on_message(filters.command('ايدي'))
 def ids(_, message):
     reply = message.reply_to_message
     if reply:
         message.reply_text(
-            f"**ʏᴏᴜʀ ɪᴅ**: `{message.from_user.id}`\n**{reply.from_user.first_name}'s ɪᴅ**: `{reply.from_user.id}`\n**ᴄʜᴀᴛ ɪᴅ**: `{message.chat.id}`"
+            f"**ايديك**: `{message.from_user.id}`\n**{reply.from_user.first_name}'s ɪᴅ**: `{reply.from_user.id}`\n**ايدي الدردشة**: `{message.chat.id}`"
         )
     else:
         message.reply(
-            f"**ʏᴏᴜʀ ɪᴅ**: `{message.from_user.id}`\n**ᴄʜᴀᴛ ɪᴅ**: `{message.chat.id}`"
+            f"**ايديك**: `{message.from_user.id}`\n**ايدي الدردشة**: `{message.chat.id}`"
         )
